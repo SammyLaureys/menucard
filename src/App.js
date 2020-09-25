95cba92e4f623be87c7c397012de9b4c76eaeefe
sammy <sammylaureys@hotmail.com>
2020-09-25 16:51:25 +0200

    oef 4.8 af

--- src/App.js
+++ src/App.js
@@ -7,19 +7,20 @@ const PRODUCTS_DATA = [
         products: [
             {name: "cola", price: 2},
             {name: "water", price: 2},
-            {name: "bier", price: 2},
+            {name: "bier", price: 2, size: 25},
             {name: "wijn", price: 4}
         ],
     },
     {
         name: "eten",
+        note: "een klein hapje voor de snelle honger",
         products: [
-            {name: "chips", price: 1},
-            {name: "olijven", price: 2},
-            {name: "kaas", price: 4},
+            {name: "chips", price: 1, note: "paprika of zout"},
+            {name: "olijven", price: 2, note: "mix zwart en groen"},
+            {name: "kaas", price: 4, note: "mix jonge en oude blokjes"},
             {name: "salami", price: 3},
-            {name: "hamburger", price: 4},
-            {name: "pizza", price: 4}
+            {name: "hamburger", price: 4, note: "niet na 22:00"},
+            {name: "pizza", price: 4, note: "niet na 22:00"}
         ]
     }
 ];
@@ -28,6 +29,7 @@ function Category(props) {
     const {category} = props;
     return <div>
         <h2>{category.name}</h2>
+        <p className="note">{category.note}</p>
         {category.products.map((p) => <ProductLine key={p.name} product={p}/>)}
     </div>
 
@@ -35,10 +37,16 @@ function Category(props) {
 
 function ProductLine(props) {
     const {product} = props;
-    return <div className="ProductLine">
-        <div>{product.name}</div>
-        <div>{product.price}&euro;</div>
-    </div>;
+    return <div>
+        <div className="ProductLine">
+            <div>{product.name}</div>
+            <div>{product.price}&euro;</div>
+            <div className="productSize">{product.size}</div>
+        </div>
+        <div className="note productNote">{product.note}</div>
+    </div>
+        ;
+
 }
 
 function App() {
@@ -47,7 +55,7 @@ function App() {
             <h1>Menu</h1>
             <hr/>
             {
-                PRODUCTS_DATA.map( (c) =>
+                PRODUCTS_DATA.map((c) =>
                     <Category key={c.name} category={c}/>
                 )
 
