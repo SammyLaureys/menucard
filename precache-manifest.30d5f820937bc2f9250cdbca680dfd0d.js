bea22e2ace92e2eb7c75fbf898d1a0309a751571
sammy <sammylaureys@hotmail.com>
2020-09-26 16:52:39 +0200

    Updates

---
+++ precache-manifest.30d5f820937bc2f9250cdbca680dfd0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03389d2d6c87c3e51911c4e7742d089",
+    "url": "/menucard/index.html"
+  },
+  {
+    "revision": "f29e7ae4ce468a9715c2",
+    "url": "/menucard/static/css/main.77b77d94.chunk.css"
+  },
+  {
+    "revision": "258072d1d49380af24aa",
+    "url": "/menucard/static/js/2.e8e4106a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menucard/static/js/2.e8e4106a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f29e7ae4ce468a9715c2",
+    "url": "/menucard/static/js/main.6609495f.chunk.js"
+  },
+  {
+    "revision": "39f5a1cd608593fdedcb",
+    "url": "/menucard/static/js/runtime-main.52e723cb.js"
+  }
+]);
