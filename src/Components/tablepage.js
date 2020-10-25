992afeaf617ecee1fcba5f94d01faa70ca42b47d
sammy <sammylaureys@hotmail.com>
2020-10-25 16:55:07 +0100

    url navigation to table number possible

--- src/Components/tablepage.js
+++ src/Components/tablepage.js
@@ -2,7 +2,7 @@ import React from "react";
 import {useParams} from "react-router-dom"
 
 export function TablePage(){
-    const tableNumberFromUrl = useParams();
+    const {tableNumberFromUrl} = useParams();
     return <div>
         Welkom bij tafel {tableNumberFromUrl}!!
     </div>
